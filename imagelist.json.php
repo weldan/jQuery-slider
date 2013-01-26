5f9ea3ce900e60ee87f86c97fb10d98c8c1be527
Weldan Jamili <mweldan@gmail.com>
2013-01-26 17:07:15 +0800

    now can use json list as data source, put example for using with json data source

---
+++ imagelist.json.php
@@ -0,0 +1,13 @@
+<?php
+$images = array(
+   array('<img src="http://i.imgur.com/mXc4xdb.jpg" alt="">'),
+   array('<img src="http://i.imgur.com/QrDBx.jpg" alt="">'),
+   array('<img src="http://i.imgur.com/0cjnd.jpg" alt="">'),
+   array('<img src="http://i.imgur.com/K9O3H.jpg" alt="">'),
+   array('<img src="http://i.imgur.com/Dln66.gif" alt="">'),
+   array('<img src="http://i.imgur.com/IHQlz.jpg" alt="">'),
+   array('<img src="http://i.imgur.com/MQHYB.jpg" alt="">') 
+);
+echo json_encode($images);
+exit;
+?>
