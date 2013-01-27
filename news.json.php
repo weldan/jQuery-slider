03c72f9617dc892db2dacce6ee2d35c36541015c
Weldan Jamili <mweldan@gmail.com>
2013-01-27 18:53:14 +0800

    add new example

---
+++ news.json.php
@@ -0,0 +1,43 @@
+<?php
+$news[] = array('
+    <div class="contents">
+    <h1>News</h1>
+    <h3>Kucing Terbang Ditemui</h3>
+    <p>Seekor kucing ditemui terbang di KLIA semalam, dipercayai melarikan diri dari penjual ikan.
+    Beliau ketika ditemui berkata: "I believe I can fly"</p>    
+    </div>
+');
+$news[] = array('
+    <div class="contents">
+    <h1>News</h1>
+    <h3>Basikal Fixie Masuk Bursa Saham</h3>
+    <p>Basikal Fixie kini di bursa saham Malaysia, dengan indeks peratusan 0.01</p>   
+    </div>
+');
+$news[] = array('
+    <div class="contents">
+    <h1>News</h1>
+    <h3>Maharaja Lawak Kurang Lawak</h3>
+    <p>Seorang penonton televisyen sepenuh masa merungut Maharaja Lawak penuh dengan lawak yang tidak berapa 
+    lawak.</p> 
+    </div>
+');
+$news[] = array('
+    <div class="contents">
+    <h1>News</h1>
+    <h3>Perodua Hasil Motor Roda Tiga Dengan Kuasa Solar</h3>
+    <p>Perodua subuh tadi memperkenalkan hasil ciptaan baru mereka, motor roda tiga dengan 
+    kuasa solar.</p>
+    </div>
+');
+$news[] = array('
+    <div class="contents">
+    <h1>News</h1>
+    <h3>Kumpulan Kugiran Potong Rambut Lagi</h3>
+    <p>Akibat diharamkan masuk radio kumpulan gitar rancak memotong rambut mereka lagi, flat top</p>
+    </div>
+');
+
+echo json_encode($news);
+exit;
+?>
